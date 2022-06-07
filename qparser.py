bedad1fa5a7b0d8e8c26b232b0ee8b2083030ee5
vlad-bb <96999713+vlad-bb@users.noreply.github.com>
2022-06-07 22:46:50 +0300

    commit

--- qparser.py
+++ qparser.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 
 
-
 def main():
     driver = webdriver.Chrome(executable_path='https://selenoid.to-3d.com/status')
-    driver.get('https://www.gauthmath.com/solution/i1130273393/A-motor-boat-takes4hours-to-travel-128-miles-going-upstream-The-return-trip-take')
-    answer = driver.find_element_by_class_name('in102').text
-    return  answer
+    driver.get('https://www.gauthmath.com/solution/i1130273393/A-motor-boat-takes4hours-to-travel-128-miles-going'
+               '-upstream-The-return-trip-take')
+    answer = driver.find_element_by_xpath('/html/head/meta[1]').get_attribute('content')
+
+    return answer
 
 if __name__ == '__main__':
     main()
+
+
