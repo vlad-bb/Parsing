209fcf1170c0ac8313ee4adc592b43a8a6b44d11
Vladyslav Babenko <96999713+vlad-bb@users.noreply.github.com>
2023-01-20 09:08:54 +0200

    commit

--- old_files/draft3.py
+++ old_files/draft3.py
@@ -13,8 +13,27 @@
 #     finally:
 #         print('-' * 30)
 
-some = set()
-for _ in range(10):
-    i = 'Title'
-    some.add(i)
-print(some)
+# some = set()
+# for _ in range(10):
+#     i = 'Title'
+#     some.add(i)
+# print(some)
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ottawa.bibliocommons.com/explore/bestsellers/5925_new_york_times'
+html_doc = requests.get(url)
+
+file = "old_files/example.html"
+with open(file, "w") as fh:
+    fh.write(html_doc.text)
+with open(file) as fr:
+    response = fr.read()
+
+soup = BeautifulSoup(response, 'html.parser')
+# print(soup)
+books = soup.find('div', class_='cp_bib_list clearfix').find_all('div', class_='col-xs-12 list_item_outer clearfix')
+
+for book in books:
+    print(book.find('span', class_="title").text)
+    print('-'*60)
